7ff363c1560e1b7d628b2d2096cb02daa3e43287
kausarm <muhammadkausar01@gmail.com>
2022-05-24 12:07:54 +0700

    tampil react tailwind webinar template

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-gray-800 App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-red-400'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        borderC: '#0B7142',
+        primaryC: '#2C9C54',
+        secondaryC: '#FAB421',
+        sRendah: '#ED4242',
+      },
+    },
+  },
+  plugins: [],
+};
